899b1194e42b086459fa2705ec3606eff18b3f85
Kristján Oddsson <koddsson@gmail.com>
2023-06-08 16:40:45 +0200

    Try fixing site

--- _data/sites/koddsson.js
+++ _data/sites/koddsson.js
@@ -1,9 +1,7 @@
 module.exports = {
 	name: "koddsson.com", // optional, falls back to object key
 	description: "Kristján's Personal web site",
-	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: false,
 	options: {
 		frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
